e6cbded2488fffdc00d181e5df23c6932ef8fc37
R3n470S1lv4 <renato.349.709@gmail.com>
2024-07-08 12:23:58 -0300

    multiplas execucoes e escopo

--- src/main/java/com/batch/processing/BatchConfig.java
+++ src/main/java/com/batch/processing/BatchConfig.java
@@ -3,10 +3,14 @@ package com.batch.processing;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -19,6 +23,7 @@ public class BatchConfig {
   public Job ImprimeOla(JobRepository jobRepository, Step imprimeOlaStep) {
     return new JobBuilder("ImprimeOla", jobRepository)
         .start(imprimeOlaStep)
+        .incrementer(new RunIdIncrementer())
         .build();
   }
 
@@ -26,11 +31,17 @@ public class BatchConfig {
   public Step imprimeOlaStep(JobRepository jobRepository,
       PlatformTransactionManager platformTransactionManager) {
     return new StepBuilder("imprimeOlaStep", jobRepository)
-        .tasklet((contribution, chunkContext) -> {
-          System.out.println("ola, mundo!");
-          return RepeatStatus.FINISHED;
-        }, platformTransactionManager)
+        .tasklet(imprimeOlaMundoTasklet(null), platformTransactionManager)
         .build();
+  }
+
+  @Bean
+  @StepScope
+  public Tasklet imprimeOlaMundoTasklet(@Value("#{jobParameters['nome']}") String nome) {
+    return (contribution, chunkContext) -> {
+      System.out.println(String.format("ola, %s!", nome));
+      return RepeatStatus.FINISHED;
+    };
   }
 
 }
